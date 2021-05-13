b7f3b9caff3a90471deb16799e4f3daf472fb5d9
Jason Sakuwa <phantythephantump@gmail.com>
2021-05-13 23:55:18 +0900

    Commented oiut alpha, and increased button size

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 randomColor = () => {
-    if (document.querySelector("#alpha").checked) {
-        const r = Math.floor(Math.random() * 255 + 1);
-        const g = Math.floor(Math.random() * 255 + 1);
-        const b = Math.floor(Math.random() * 255 + 1);
-        const a = Math.floor(Math.random() * 1000) / 1000;
-        document.querySelector("body").style.backgroundColor = "rgb(".concat(r, ",", g, ",", b, ",", a, ")");    
-    } else {
+    // if (document.querySelector("#alpha").checked) {
+    //     const r = Math.floor(Math.random() * 255 + 1);
+    //     const g = Math.floor(Math.random() * 255 + 1);
+    //     const b = Math.floor(Math.random() * 255 + 1);
+    //     const a = Math.floor(Math.random() * 1000) / 1000;
+    //     document.querySelector("body").style.backgroundColor = "rgb(".concat(r, ",", g, ",", b, ",", a, ")");    
+    // } else {
         const r = Math.floor(Math.random() * 255 + 1);
         const g = Math.floor(Math.random() * 255 + 1);
         const b = Math.floor(Math.random() * 255 + 1);
         document.querySelector("body").style.backgroundColor = "rgb(".concat(r, ",", g, ",", b, ")");
-    }
+    // }
 }
